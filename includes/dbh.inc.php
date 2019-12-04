8692c8d99f0c91ffea81778a82f7b772b40c0bf1
rsleeper1 <39035524+rsleeper1@users.noreply.github.com>
2019-12-03 18:24:05 -0600

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -19,6 +19,19 @@ if (!$conn) {
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
 
+
+ $sql = "SELECT * FROM easyquestions, tips WHERE easyquestions.Tip_ID = tips.Tip_ID AND E_Question_ID=1;";
+            $result = mysqli_query($conn, $sql);
+            $resultCheck = mysqli_num_rows($result);
+            if ($resultCheck > 0) {
+                while ($row = mysqli_fetch_assoc($result)){
+                    echo $row['E_Question'] . "<br>";
+                    $questionTip = $row['Tip'];
+                    $answer = $row['E_Answer'];
+                }
+            }
+
+
 mysqli_close($conn);
 ?>
 
