9178f78bb7a1ae6a76ad1dae17c3679221141242
rsleeper1 <39035524+rsleeper1@users.noreply.github.com>
2019-12-03 18:19:28 -0600

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -7,5 +7,20 @@ $password = $url["625b6900"];
 $db = substr($url["heroku_dd8fcef5d8ead47"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
+
+
+if (!$conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
+
+mysqli_close($conn);
 ?>
+
+
 
